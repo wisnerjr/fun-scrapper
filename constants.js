6637f8a62c1b70abde975f294457db3f2ae5e3a6
Wisner Júnior <wisnerjr@gmail.com>
2019-04-13 16:13:00 -0300

    scrapping quotes/author/tags of the first page

--- constants.js
+++ constants.js
@@ -6,7 +6,7 @@ export default {
 }
 
 
-// export const QUOTE = 'body > div > div:nth-child(2) > div.col-md-8 > div:nth-child(1)';
-// export const QUOTE_TEXT = 'body > div > div:nth-child(2) > div.col-md-8 > div:nth-child(1) > span.text';
-// export const QUOTE_AUTHOR = 'body > div > div:nth-child(2) > div.col-md-8 > div:nth-child(1) > span:nth-child(2) > small';
-// export const QUOTE_TAG = 'body > div > div:nth-child(2) > div.col-md-8 > div:nth-child(1) > div > a:nth-child(2)';
+export const QUOTE = 'body > div > div:nth-child(2) > div.col-md-8 > div:nth-child(1)';
+export const QUOTE_TEXT = 'body > div > div:nth-child(2) > div.col-md-8 > div:nth-child(1) > span.text';
+export const QUOTE_AUTHOR = 'body > div > div:nth-child(2) > div.col-md-8 > div:nth-child(1) > span:nth-child(2) > small';
+export const QUOTE_TAG = 'body > div > div:nth-child(2) > div.col-md-8 > div:nth-child(1) > div > a:nth-child(2)';
--- index.js
+++ index.js
@@ -2,21 +2,42 @@
 // import QUOTE from './constants';
 
 const QUOTE = 'body > div > div:nth-child(2) > div.col-md-8 > div:nth-child(1)';
+const QUOTE_TEXT = 'body > div > div:nth-child(2) > div.col-md-8 > div:nth-child(1) > span.text';
 
 const puppeteer = require('puppeteer');
 
 const url = 'http://quotes.toscrape.com/';
 
+let quotes = [];
+
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1920, height: 926 });
-    await page.goto(url);
+    try {
 
-    const quotesPerPage = await page.$eval(QUOTE, el => { return {...el}});
+        const browser = await puppeteer.launch({
+            headless: false,
+            args: [
+                '--start-maximized'
+            ]
+        });
+        const page = await browser.newPage();
 
-    console.log(quotesPerPage);
+        await page.goto(url);
 
-    await browser.close();
-   
+        let quotesPerPage = await page.evaluate(() => {
+            return Array.from(document.querySelectorAll('.quote'), el => {
+                return {
+                    quote: el.querySelector('span.text').innerText,
+                    author: el.querySelector('span small.author').innerText,
+                    tags: Array.from(el.querySelectorAll('div.tags a.tag'), tag  => tag.innerText)
+                }
+            });
+        });
+
+       quotesPerPage.forEach(quote => quotes.push(quote));
+
+        console.log(quotes);
+
+    } catch (e) {
+        console.log(e);
+    }
 })();
